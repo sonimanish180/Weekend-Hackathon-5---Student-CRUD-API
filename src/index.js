d4d8e8a14330afc53ca1461e537ae98c1a7d6249
Manish Soni <msoni@ar.iitr.ac.in>
2020-11-19 21:45:06 +0530

    chnages1

--- src/index.js
+++ src/index.js
@@ -3,13 +3,110 @@ const app = express()
 const bodyParser = require("body-parser");
 const port = 8080
 app.use(express.urlencoded());
+const Joi = require('joi');
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // your code goes here
+const students = require('./InitialData').studentArray;
 
+app.get('/api/student', (req, res) => {
+    res.send(students);
+});
+
+app.get('/api/student/:id', (req, res) => {
+    const id = req.params.id;
+
+    const student = students.find(student => student.id === parseInt(id));
+
+    if(!student) {
+        res.status(404).send(`Student with id ${id} not found!`);
+        return;
+    }
+
+    res.send(student);
+});
+
+app.post('/api/v1/students', (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(1).required(),
+        subject: Joi.string().min(4).required()        
+    })
+
+    const validationObject = schema.validate(req.body);
+
+    if (validationObject.error){
+        res.status(400).send(validationObject.details[0].message);
+        return;
+    }
+
+    const student = {
+        id : students.length + 1,
+        name : req.body.name,
+        subject : req.body.subject
+    }
+
+    students.push(student);
+
+    console.log(req.body);
+    res.send(student);
+});
+
+app.put('/api/v1/students/:id', (req, res) => {
+    const id = req.params.id;
+    const schema = Joi.object({
+        name: Joi.string().min(1).required(),
+        subject: Joi.string().min(4).required()        
+    })
+
+    const validationObject = schema.validate(req.body);
+
+    if (!id) {
+        res.status(404).send('Student Id is Required.!');
+        return;
+    }
+
+    if (validationObject.error) {
+        res.status(400).send(validationObject.error.details[0].message);
+        return;
+    }
+
+    let student = students.find(student => student.id === parseInt(id));
+
+    student.name = req.body.name;
+    student.subject = req.body.subject;
+
+    console.log(student);
+
+    res.send(student);
+    
+});
+
+app.delete('/api/students/:id', (req, res) => {
+    const id = req.params.id;
+
+
+    if (!id) {
+        res.status(404).send('Student Id is Required.!');
+        return;
+    }
+
+    const studentIndex = students.findIndex(student => student.id === parseInt(id));
+    
+    if (studentIndex === -1) {
+        res.status(404).send('Student not found!');
+        return;
+    }
+
+    const student = students[studentIndex];
+
+    students.splice(studentIndex, 1);
+
+    res.send(students);
+    
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
